f8d52f21787dc41a1b2d2fbcc13ed16e816f7077
gregbg <hvgreg01@gmail.com>
2022-01-01 00:50:20 +0530

    Feature : implementing logging
    1. Added log4j dependencies.
    2. configured logging

---
+++ src/main/java/bootstrap/Driver.java
@@ -0,0 +1,48 @@
+package bootstrap;
+
+
+import org.apache.log4j.DailyRollingFileAppender;
+import org.apache.log4j.EnhancedPatternLayout;
+import org.apache.log4j.Level;
+import org.apache.log4j.Priority;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class Driver {
+
+
+    static Logger logger = LoggerFactory.getLogger(Driver.class);
+
+    public static void main(String[] args) throws Exception
+    {
+        configureLogging("gitIntroLogs", false);
+        System.out.println("Hello world");
+    }
+
+
+    public static String configureLogging(String logDirectory, boolean debug) {
+        DailyRollingFileAppender dailyRollingFileAppender = new DailyRollingFileAppender();
+
+        String logFileName = "";
+        if (!debug) {
+            dailyRollingFileAppender.setThreshold(Level.toLevel(Priority.INFO_INT));
+            logFileName = logDirectory + "gitIntro.log";
+
+        } else {
+            dailyRollingFileAppender.setThreshold(Level.toLevel(Priority.DEBUG_INT));
+            logFileName = logDirectory + "gitIntro-debug.log";
+        }
+
+        System.out.println("Log files written out at " + logFileName);
+        dailyRollingFileAppender.setFile(logFileName);
+        dailyRollingFileAppender.setLayout(new EnhancedPatternLayout("%-6d [%t] %-5p %c - %m%n"));
+
+        dailyRollingFileAppender.activateOptions();
+        org.apache.log4j.Logger.getRootLogger().addAppender(dailyRollingFileAppender);
+        return dailyRollingFileAppender.getFile();
+    }
+
+
+}
